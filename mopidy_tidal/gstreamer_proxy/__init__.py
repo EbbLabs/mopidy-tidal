c360c68cef2c6da9c6fe37efd0533f4e8bc4892e
John Morris <2e0byo@gmail.com>
2026-01-01 16:35:28 +0100

    fix: construct cache exactly once.

--- mopidy_tidal/gstreamer_proxy/__init__.py
+++ mopidy_tidal/gstreamer_proxy/__init__.py
@@ -1,3 +1,4 @@
+import functools
 import sqlite3
 from pathlib import Path
 
@@ -5,6 +6,7 @@ from .cache import SQLiteCache
 from .proxy import ProcessProxy, Proxy, ProxyConfig
 
 
+@functools.cache
 def mopidy_track_cache(path: Path) -> ProcessProxy:
     path.parent.mkdir(parents=True, exist_ok=True)
     proxy = Proxy(
